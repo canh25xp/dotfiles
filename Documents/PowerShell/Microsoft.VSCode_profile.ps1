34d39ed7742525afd89848b3d0d196decd4a7c18
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-13 22:16:51 +0700

    Update Documents/PowerShell/Microsoft.VSCode_profile.ps1

--- Documents/PowerShell/Microsoft.VSCode_profile.ps1
+++ Documents/PowerShell/Microsoft.VSCode_profile.ps1
@@ -1,13 +1,11 @@
+# The Default Prompt
 # function Prompt {
 #     $env:USERNAME+"@"+$env:COMPUTERNAME+":"+"$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
 # }
 
-$GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
-
+# Shell Prompt
 function Prompt {
-    # Your non-prompt logic here
     $prompt = Write-Prompt "$env:USERNAME@$env:COMPUTERNAME" -ForegroundColor ([ConsoleColor]::Green)
-    $prompt += ":"
-    $prompt += & $GitPromptScriptBlock
+    $prompt += ":$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
     $prompt
 }
