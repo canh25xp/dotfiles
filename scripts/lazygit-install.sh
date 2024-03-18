339b7937aa17376670fca546115ecda5942b8dc4
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-18 15:12:23 +0700

    edit scripts

--- scripts/lazygit-install.sh
+++ scripts/lazygit-install.sh
@@ -3,3 +3,5 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+rm lazygit
+rm lazygit.tar.gz
