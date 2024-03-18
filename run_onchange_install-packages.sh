bfc0840a78110815655947cbebf9bf90be24b261
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-18 13:10:33 +0700

    add scripts

---
+++ run_onchange_install-packages.sh
@@ -0,0 +1,38 @@
+# {{ if eq .chezmoi.os "linux" }}
+
+#!/bin/sh
+
+packages=(
+	"curl"
+	"wget"
+	"wslu"
+	"vim"
+	"neofetch"
+	"git"
+	"snapd"
+	"man-db"
+	"bash-completion"
+	"ripgrep"
+	"unzip"
+	"python3"
+	"python3-pip"
+	"build-essential"
+)
+
+echo "The following packages will be installed:"
+for package in "${packages[@]}"; do
+	echo "- $package"
+done
+
+read -p "Do you want to proceed with the installation? (y/n): " confirmation
+
+if [[ "$confirmation" == "y" || "$confirmation" == "Y" ]]; then
+	# Install packages
+	sudo apt-get update
+	sudo apt-get install "${packages[@]}"
+	echo "Packages installed successfully!"
+else
+	echo "Installation canceled."
+fi
+
+# {{ end }}
