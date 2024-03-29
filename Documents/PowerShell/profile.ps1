9dacd21f4c4d6d2629757330c0bc1fc00f9ddefc
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-29 08:39:52 +0700

    Update AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json Update Documents/PowerShell/profile.ps1

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -24,6 +24,17 @@ if (Test-Path("$PSScriptRoot\PSReadLineProfile.ps1")) {
   . "$PSScriptRoot\PSReadLineProfile.ps1"
 }
 
+# Tab-copletions for winget
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+
 # Tab-completions for `git`
 # Import-Module -Name posh-git
 # $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
