11f5044523740cc6aa0b6f703142b42d75bdd5f3
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-29 15:51:29 +0700

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,7 @@
 
 # Load Oh-my-posh theme https://ohmyposh.dev/docs/themes
 # $OMPTHEME = "$env:POSH_THEMES_PATH\agnoster.minimal.omp.json"
-$OMPTHEME = "~/Posh_Themes/agnoster.minimal.omp.json"
+$OMPTHEME = "~/.config/oh-my-posh/powerline.omp.json"
 oh-my-posh init pwsh --config $OMPTHEME | Invoke-Expression
 
 # Terminal-Icons theme (https://github.com/devblackops/Terminal-Icons)
