c100acc38f440192cc4527fbe8b74033d470f338
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-31 03:23:34 +0700

    add neovide config

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,9 +3,10 @@
 # ==============================================
 
 # Load Oh-my-posh theme https://ohmyposh.dev/docs/themes
-# $OMPTHEME = "$env:POSH_THEMES_PATH\agnoster.minimal.omp.json"
-$OMPTHEME = "~/.config/oh-my-posh/powerline.omp.json"
-oh-my-posh init pwsh --config $OMPTHEME | Invoke-Expression
+oh-my-posh init pwsh --config "~/.config/oh-my-posh/powerline.omp.json" | Invoke-Expression
 
 # Terminal-Icons theme (https://github.com/devblackops/Terminal-Icons)
 Import-Module -Name Terminal-Icons
+
+# Using posh-git module for autocompletion
+$env:POSH_GIT_ENABLED = $true
