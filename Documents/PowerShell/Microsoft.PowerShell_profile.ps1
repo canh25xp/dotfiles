230279ed65eaca7fbd9254c7046198312e042eaf
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-01 11:09:21 +0700

    enable oh-my-posh only outside neovim

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,11 +2,14 @@
 # SHELL APPEARANCES
 # ==============================================
 
-# Load Oh-my-posh theme https://ohmyposh.dev/docs/themes
-oh-my-posh init pwsh --config "~/.config/oh-my-posh/powerline.omp.json" | Invoke-Expression
+# Only enable outside of neovim
+if ($env:NVIM -eq $null) {
+  # Load Oh-my-posh theme https://ohmyposh.dev/docs/themes
+  oh-my-posh init pwsh --config "~/.config/oh-my-posh/powerline.omp.json" | Invoke-Expression
+  # Using posh-git module for autocompletion
+  $env:POSH_GIT_ENABLED = $true
+}
 
 # Terminal-Icons theme (https://github.com/devblackops/Terminal-Icons)
 Import-Module -Name Terminal-Icons
 
-# Using posh-git module for autocompletion
-$env:POSH_GIT_ENABLED = $true
