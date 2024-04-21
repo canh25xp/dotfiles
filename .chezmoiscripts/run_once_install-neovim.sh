14a1d451d40a69933fe30fa08ca1f1743bb7dfea
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-21 12:20:50 +0000

    edit neovim install script

--- .chezmoiscripts/run_once_install-neovim.sh
+++ .chezmoiscripts/run_once_install-neovim.sh
@@ -2,7 +2,7 @@
 
 # https://github.com/neovim/neovim/blob/master/BUILD.md
 
-echo "Installing neovim ..."
+echo "Installing neovim..."
 
 if command -v nvim &>/dev/null; then
 	echo "Neovim is already installed. Exiting."
@@ -14,7 +14,7 @@ confirmation=${confirmation:-Y}
 
 if ! [[ "$confirmation" == "y" || "$confirmation" == "Y" ]]; then
 	echo "Installation canceled."
-  exit 0
+	exit 0
 fi
 
 mkdir -p ~/projects/lua/
@@ -29,4 +29,8 @@ sudo apt-get install ninja-build gettext cmake unzip curl build-essential file
 
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 
-cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+cd build
+
+cpack -G DEB
+
+sudo dpkg -i nvim-linux64.deb
