33fdadfa7876bf1cd9e08b72681e974ff5b1aae3
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-21 23:56:28 +0700

    edit install scripts

--- scripts/go-install.sh
+++
@@ -1,9 +0,0 @@
-# https://go.dev/doc/install
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
-
-export PATH=$PATH:/usr/local/go/bin # Add to .bash_profile
-
-go install github.com/jesseduffield/lazygit@latest
-
-env CGO_ENABLED=0 go install -ldflags="-s -w" github.com/gokcehan/lf@latest
