add45be6033a19f846147030dce3dbccdd1dac49
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-22 09:11:20 +0700

    edit gh install script

--- scripts/install-gh.sh
+++ scripts/install-gh.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo mkdir -p -m 755 /etc/apt/keyrings
 wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
