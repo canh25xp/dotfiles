c33451b71672769653e3bf04cc79c6cd7c266a81
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-04-23 02:03:50 +0700

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,14 +1,23 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
-
--- This will hold the configuration.
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
--- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("CaskaydiaCove Nerd Font")
+config.font_size = 12
 
-config.color_scheme = 'Catppuccin Mocha'
+config.enable_tab_bar = true
 
--- and finally, return the configuration to wezterm
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+-- Shell
+config.default_prog = { "pwsh", "-NoLogo" }
 
 return config
