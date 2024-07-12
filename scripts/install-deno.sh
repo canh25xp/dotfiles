e78b6e94df187bf6aa9bb5b8521ce69a55829faa
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-12 21:01:03 +0700

    add deno installing script

---
+++ scripts/install-deno.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# https://deno.com/
+
+echo "================================================================================"
+echo "[Chezmoi] Installing deno"
+
+curl -fsSL https://deno.land/install.sh | sh
