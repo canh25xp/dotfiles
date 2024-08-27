2062b3ff1aadcc0d2aaf2fd845ad28ab4c8ab523
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-27 23:55:09 +0700

    ask before install gh dash

--- scripts/install-gh-dash.sh
+++ scripts/install-gh-dash.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 # https://github.com/dlvhdr/gh-dash
 
+echo "================================================================================"
+echo "[Chezmoi] Installing gh dash extension"
+
+read -p "Do you want to proceed with the installation? (Y/n): " confirmation
+confirmation=${confirmation:-Y}
+
+if ! [[ "$confirmation" == "y" || "$confirmation" == "Y" ]]; then
+	echo "Installation canceled."
+	exit 0
+fi
+
 gh extension install dlvhdr/gh-dash
