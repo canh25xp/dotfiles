1e0cc8ceddc2c12a62fe26ca8d2a844fc3a0a4b8
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-16 20:46:39 +0700

    check executable before init oh-my-posh and zoxide

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,17 +2,25 @@
 # SHELL INIT
 # ==============================================
 
-# Only enable outside of neovim
-if ($null -eq $Env:NVIM) {
+# Init oh-my-posh outside of neovim
+$has_oh_my_posh = [bool](Get-Command -Name "oh-my-posh.exe" -ErrorAction SilentlyContinue)
+if (!$Env:NVIM -and $has_oh_my_posh) {
   # Load Oh-my-posh theme https://ohmyposh.dev/docs/themes
   oh-my-posh init pwsh --config "~/.config/oh-my-posh/catppuccin.omp.json" | Invoke-Expression
-  # Using posh-git module for autocompletion
-  $env:POSH_GIT_ENABLED = $true
+} else {
+  function Prompt {
+    #$prompt = "$env:USERNAME@$env:COMPUTERNAME "
+    $prompt += "$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
+    $prompt
+  }
 }
 
 # Init zoxide
-# (zoxide init powershell | Out-String) | Invoke-Expression
-Invoke-Expression (& { (zoxide init --cmd j powershell | Out-String) })
+$has_zoxide = [bool](Get-Command -Name "zoxide.exe" -ErrorAction SilentlyContinue)
+if ($has_zoxide) {
+  Invoke-Expression (& { (zoxide init --cmd j powershell | Out-String) })
+  # (zoxide init powershell | Out-String) | Invoke-Expression # Same as above
+}
 
 # Terminal-Icons theme (https://github.com/devblackops/Terminal-Icons)
 # Import-Module -Name Terminal-Icons
