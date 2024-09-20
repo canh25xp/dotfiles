7c0718caea6864f698c8bf0de0d92e2f81dc282c
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-21 00:22:54 +0700

    Update Documents/PowerShell/Aliases.ps1

--- Documents/PowerShell/Aliases.ps1
+++ Documents/PowerShell/Aliases.ps1
@@ -43,6 +43,31 @@ Set-Alias -Name gvim    -Value neovide
 # FUNTIONS
 # ==============================================
 
+function Set-Wallpaper {
+  param (
+      [Parameter(Mandatory = $true, Position = 0)]
+      [string]$path
+  )
+  $setwallpapersrc = @"
+  using System.Runtime.InteropServices;
+
+  public class Wallpaper {
+    public const int SetDesktopWallpaper = 20;
+    public const int UpdateIniFile = 0x01;
+    public const int SendWinIniChange = 0x02;
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+    public static void SetWallpaper(string path) {
+      SystemParametersInfo(SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange);
+    }
+  }
+"@
+
+Add-Type -TypeDefinition $setwallpapersrc
+
+[Wallpaper]::SetWallpaper($path)
+}
+
 function Edit-Config {
   # Get the list of files managed by chezmoi
   $chezmoiFiles = chezmoi managed -p absolute -i files
