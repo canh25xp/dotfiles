f5358b6fd4e0d64750a9442506b332429f97e6a4
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-22 15:12:24 +0700

    Update Documents/PowerShell/profile.ps1

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -18,8 +18,10 @@ $env:FZF_DEFAULT_OPTS = @"
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
 --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
 --color=selected-bg:#45475a
+--height=50%
 --multi
 --layout=reverse
+--info=inline
 "@
 
 $env:Path += "$HOME\Documents\PowerShell\Scripts;"
