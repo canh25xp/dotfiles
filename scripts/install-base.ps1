c00673b3eac9555d8cf2affb1f0ffa94b52142d1
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-12-25 20:18:52 +0700

    auto install fonts

--- scripts/install-base.ps1
+++ scripts/install-base.ps1
@@ -4,3 +4,16 @@ Write-Host "[Chezmoi] Install base winget packages"
 $sourcePath = chezmoi source-path
 
 winget import -i $sourcePath/scripts/winget_packages_base.json --no-upgrade
+
+$confirmation = Read-Host "Do you want to install fonts? (Y/n)"
+
+if ($confirmation -eq 'n') {
+    Write-Host "Skip fonts"
+    exit
+}
+
+$script = [scriptblock]::Create((iwr 'https://to.loredo.me/Install-NerdFont.ps1'))
+
+$fontList = & $script -List All
+
+& $script -Confirm:$false -Name Cascadia-Code
