7349b8055081dfd7a5175eede8e1725d67406531
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-02-28 23:14:45 +0700

    scripts: edit setenv script

--- .chezmoiscripts/run_once_wslenv.ps1
+++
@@ -1,5 +0,0 @@
-# batch style
-# setx WSLENV "USERPROFILE/p:LOCALAPPDATA/p"
-
-# powershell style
-[Environment]::SetEnvironmentVariable("WSLENV", "USERPROFILE/p:LOCALAPPDATA/p", [System.EnvironmentVariableTarget]::User)
