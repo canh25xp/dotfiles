8d2cc766cff19db64a5ed4ba400aa2961d9dd6c7
Ngô Văn Cảnh <vancanh-ng@samsung.com>
2025-06-18 09:43:03 +0700

    powershell: VSCode_profile source PSReadLineProfile, Aliases

--- Documents/PowerShell/Microsoft.VSCode_profile.ps1
+++ Documents/PowerShell/Microsoft.VSCode_profile.ps1
@@ -1,11 +1,17 @@
+$profileRoot = $PROFILE | Split-Path
+
+. $profileRoot\Aliases.ps1
+
+. $profileRoot\PSReadLineProfile.ps1
+
 # The Default Prompt
 # function Prompt {
 #     $env:USERNAME+"@"+$env:COMPUTERNAME+":"+"$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
 # }
 
 # Shell Prompt
-function Prompt {
-    $prompt = Write-Prompt "$env:USERNAME@$env:COMPUTERNAME" -ForegroundColor ([ConsoleColor]::Green)
-    $prompt += ":$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
-    $prompt
-}
+# function Prompt {
+#     $prompt = Write-Prompt "$env:USERNAME@$env:COMPUTERNAME" -ForegroundColor ([ConsoleColor]::Green)
+#     $prompt += ":$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
+#     $prompt
+# }
