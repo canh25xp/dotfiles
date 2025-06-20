7fb1bb0e51393902dfdc00672d3f25c44d1a32f8
Ngô Văn Cảnh <vancanh-ng@samsung.com>
2025-06-20 09:09:50 +0700

    powershell: add function and alias to manage RecycleBin

--- .chezmoitemplates/pwsh/Aliases.ps1
+++ .chezmoitemplates/pwsh/Aliases.ps1
@@ -34,6 +34,8 @@ Set-Alias -Name wifi -Value Get-Wifi
 Set-Alias -Name wm -Value Start-Komorebi
 Set-Alias -Name word -Value Open-WinWord
 Set-Alias -Name wtf -Value Get-Command
+Set-Alias -Name yeet -Value Move-ToRecycleBin
+Set-Alias -Name nuke -Value Clear-RecycleBin
 
 # Abbreviated aliases
 Set-Alias -Name cz -Value chezmoi
@@ -386,4 +388,70 @@ function Get-CmdletAlias ($cmdletname) {
 
 function Show-Documents {
   glow $env:USERPROFILE\Documents\CheatSheets\
+}
+
+#https://www.powershellgallery.com/packages/PoshFunctions/2.2.11/Content/Functions%5CMove-ToRecycleBin.ps1
+function Move-ToRecycleBin {
+<#
+.SYNOPSIS
+    Instead of outright deleting a file, why not move it to the Recycle Bin?
+.DESCRIPTION
+    Instead of outright deleting a file, why not move it to the Recycle Bin?
+    Function aliased to 'Recycle'
+.PARAMETER Path
+    A string or array of strings representing a file or a folder. Wildcards are
+    acceptable and will be resolved to specific file or folder names. Can accept
+    values from the pipeline.
+.EXAMPLE
+    Move-ToRecycleBin -Path c:\temp\dummyfile.txt -Verbose
+ 
+    VERBOSE: Moving 'c:\temp\dummyfile.txt' to the Recycle Bin
+.EXAMPLE
+    Move-ToRecycleBin -Path c:\temp\dummyfile2.txt
+ 
+    Would move c:\temp\dummyfile2.txt to the Recycle Bin
+.EXAMPLE
+    Move-ToRecycleBin .\FileDoesNotExist
+ 
+    Move-ToRecycleBin : ERROR: Path [.\FileDoesNotExist] does not exist
+.EXAMPLE
+    Move-ToRecycleBin -Path 'File1.txt', 'File2.txt'
+ 
+    Would move both File1.txt and File2.txt to the Recycle Bin
+#>
+
+    [CmdletBinding(ConfirmImpact = 'Medium')]
+    [alias('Recycle')]
+    param (
+        [Parameter(Mandatory, HelpMessage='Please enter a path to a file or folder. Wildcards accepted.', ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string[]] $Path
+    )
+
+    begin {
+        $FileSystem = New-Object -TypeName 'Microsoft.VisualBasic.FileIO.FileSystem'
+        Write-Verbose -Message "Starting [$($MyInvocation.MyCommand)]"
+    }
+
+    process {
+        foreach ($currentPath in $Path) {
+            if (Test-Path -Path $currentPath) {
+                $File = Resolve-Path -Path $currentPath
+                foreach ($currentFile in $File) {
+                    Write-Verbose -Message ("Moving '{0}' to the Recycle Bin" -f $currentFile)
+                    if (Test-Path -Path $currentFile -PathType Container) {
+                        $FileSystem::DeleteDirectory($currentFile, 'OnlyErrorDialogs', 'SendToRecycleBin')
+                    } else {
+                        $FileSystem::DeleteFile($currentFile, 'OnlyErrorDialogs', 'SendToRecycleBin')
+                    }
+                }
+            } else {
+                Write-Error -Message "ERROR: Path [$currentPath] does not exist"
+            }
+        }
+    }
+
+    end {
+        Remove-Variable -Name FileSystem
+        Write-Verbose -Message "Ending [$($MyInvocation.MyCommand)]"
+    }
 }
