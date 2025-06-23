6303c788f78db6925f28445aa439f5b658da9314
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-06-23 22:37:17 +0700

    scripts: use batcat if bat not found

--- .chezmoiscripts/run_onchange_after_build-bat-cache.sh
+++ .chezmoiscripts/run_onchange_after_build-bat-cache.sh
@@ -3,17 +3,20 @@
 echo "================================================================================"
 echo "[Chezmoi] Building bat theme (catppuccin)"
 
-if ! command -v bat &>/dev/null ; then
-	echo "bat is not installed. Exiting"
-	exit 1
+if command -v bat &>/dev/null; then
+    BAT_CMD="bat"
+elif command -v batcat &>/dev/null; then
+    BAT_CMD="batcat"
+else
+    echo "Neither 'bat' nor 'batcat' is installed. Exiting."
+    exit 1
 fi
 
 read -p "Build bat theme? (Y/n): " confirmation
 confirmation=${confirmation:-Y}
-
-if ! [[ "$confirmation" == "y" || "$confirmation" == "Y" ]]; then
+if ! [[ "$confirmation" =~ ^[Yy]$ ]]; then
 	echo "Canceled."
 	exit 0
 fi
 
-bat cache --build
+"$BAT_CMD" cache --build
