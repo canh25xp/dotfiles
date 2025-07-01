ccb0c6989eb19b47d5b0df33286596611d871d17
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-07-01 22:26:23 +0700

    nvim: add chezmoi execute command

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -38,6 +38,8 @@ if vim.g.neovide then
   require("config.neovide")
 end
 
+require("common.chezmoi").setup({})
+
 if vim.fn.executable("nvr") == 1 then
   vim.env.GIT_EDITOR = "nvr -l --remote-wait +'set bufhidden=delete'"
 end
