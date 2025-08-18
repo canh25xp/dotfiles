43e3e262084814afba5c8904552d1610f88c3ef1
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-08-18 21:47:44 +0700

    scripts: modify winget_packages before install

--- .chezmoiscripts/run_onchange_before_00-winget-install.ps1
+++ .chezmoiscripts/run_onchange_before_00-winget-install.ps1
@@ -4,9 +4,22 @@ Write-Host "[Chezmoi] Install winget packages"
 $confirmation = Read-Host "Do you want to install winget packages? (Y/n)"
 
 if ($confirmation -eq 'n') {
-    Write-Host "exit"
-    exit
+  Write-Host "Skip"
+  exit
 }
 
-winget import $HOME/Documents/winget/winget_packages.json --no-upgrade --disable-interactivity
+$wingetPackages = "$HOME\Documents\winget\winget_packages.json"
 
+$editConfirmation = Read-Host "Do you want to edit the winget_packages.json file before installing? (Y/n)"
+
+if ($editConfirmation -ne 'n') {
+  if (Get-Command nvim -ErrorAction SilentlyContinue) {
+    nvim $wingetPackages
+  }
+  else {
+    # Wait for notepad to close
+    Start-Process notepad -ArgumentList $wingetPackages -Wait
+  }
+}
+
+winget import $wingetPackages --no-upgrade --disable-interactivity
