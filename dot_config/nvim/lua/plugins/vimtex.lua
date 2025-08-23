201b1b47f19bc28fbe94742a11e5a25bf50f6af7
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-08-23 11:45:39 +0700

    nvim: vimtex: disable if latexmk not found

--- dot_config/nvim/lua/plugins/vimtex.lua
+++ dot_config/nvim/lua/plugins/vimtex.lua
@@ -1,6 +1,7 @@
 return {
   "lervag/vimtex",
   lazy = false, -- lazy-loading will disable inverse search
+  cond = function() return vim.fn.executable("latexmk") == 1 end,
   config = function()
     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
