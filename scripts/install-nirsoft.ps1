113c23c4afef46365fc06730821a0e26a68c05ec
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-01-13 13:07:03 +0000

    Remove unused scripts

--- scripts/install-nirsoft.ps1
+++
@@ -1,3 +0,0 @@
-Set-Location $env:PROGRAMFILES
-curl -LO https://www.nirsoft.net/utils/nircmd-x64.zip
-7z x nircmd-x64.zip -oNirCmd
--- scripts/ls_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pkg install clang lua-language-server stylua ruff texlab shfmt taplo
-
-npm install -g @fsouza/prettierd
-npm install -g pyright
-npm install -g vscode-langservers-extracted
-npm install -g markdownlint-cli
