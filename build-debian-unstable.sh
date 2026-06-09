50a906a4f3c139817a9c42fabae8bf210e5bf19b
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-06-09 14:16:39 +0700

    script: add build debian unstable script

---
+++ build-debian-unstable.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Dependency check
+if command -v docker >/dev/null 2>&1; then
+    echo "docker found"
+    docker -v
+else
+    echo "docker not found! Please install docker and run this script again!"
+    exit
+fi
+
+if command -v awk >/dev/null 2>&1; then
+    echo "awk found"
+else
+    echo "awk not found! Please install awk and run this script again!"
+    exit
+fi
+
+# set dir for tars
+export TAR_DIR="$PWD/tars"
+
+# Create tar dir
+mkdir -p "$TAR_DIR"
+
+# debian
+# debian:stable-slim
+# debian:unstable
+# debian:unstable-slim
+DISTROS=debian:unstable
+
+docker run -t "$DISTROS" sh -c echo
+containerID=$(docker container ls -a | grep -i "$DISTROS" | awk '{print $1}')
+TAR_NAME=$(echo "$DISTROS" | tr '/' '_')-$(date -u +%d%m%Y%I%M).tar
+docker export "$containerID" >"$TAR_DIR"/"$TAR_NAME"
+# Remove the docker image to avoid piling up of containers
+docker rm "$containerID"
